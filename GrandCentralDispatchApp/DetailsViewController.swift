b9b2dec37708f5e1e01a9a7b705b913fc7ef862e
Zaki <menza.zm@gmail.com>
2023-08-09 09:25:55 +0600

    GCD

--- GrandCentralDispatchApp/DetailsViewController.swift
+++ GrandCentralDispatchApp/DetailsViewController.swift
@@ -8,22 +8,106 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    private let imageView = UIImageView()
+    private let activityIndicator = UIActivityIndicatorView()
+    
+    fileprivate var imageURl: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        } set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        setLayoutSubviews()
+        stylize()
+        setActions()
     }
-    */
-
+    
+    func addSubviews() {
+        view.addSubview(imageView)
+        imageView.addSubview(activityIndicator)
+    }
+    
+    func setLayoutSubviews() {
+        var layoutConstraints: [NSLayoutConstraint] = []
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        layoutConstraints += [
+            imageView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            imageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        layoutConstraints += [
+            activityIndicator.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: imageView.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(layoutConstraints)
+    }
+    
+    func stylize() {
+        view.backgroundColor = .white
+        
+        imageView.backgroundColor = .gray
+    }
+    
+    func setActions() {
+        fetchImage()
+        delay(3) {
+            self.addAlert()
+        }
+    }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay), execute: closure)
+    }
+    
+    fileprivate func addAlert() {
+        let alertController = UIAlertController(title: "Registration", message: "If you are not registered yet, please register", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        alertController.addTextField { (usernameTF) in
+            usernameTF.placeholder = "Enter your username"
+        }
+        
+        alertController.addTextField { (passwordTF) in
+            passwordTF.placeholder = "Enter your password"
+            
+        }
+        
+        self.present(alertController, animated: true)
+    }
+    
+    fileprivate func fetchImage() {
+        imageURl = URL(string: "https://images.squarespace-cdn.com/content/v1/58b4791ad2b857c893179e34/1537971642021-LHW76T7O8JG0M4GLTSTP/IMG_2818.jpg?format=500w")
+        
+        activityIndicator.startAnimating()
+        activityIndicator.isHidden = false
+        
+        let queue = DispatchQueue.global(qos: .utility)
+        
+        queue.async {
+            guard let url = self.imageURl, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
+    }
 }
--- GrandCentralDispatchApp/MainViewController.swift
+++ GrandCentralDispatchApp/MainViewController.swift
@@ -9,13 +9,52 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    private let nextPageButton = UIButton()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+        setLayoutSubviews()
+        stylize()
+        setActions()
+    
+    }
+    func addSubviews() {
+        view.addSubview(nextPageButton)
+    }
+    
+    func setLayoutSubviews() {
+        var layoutConstraints: [NSLayoutConstraint] = []
+        
+        nextPageButton.translatesAutoresizingMaskIntoConstraints = false
+        layoutConstraints += [
+            nextPageButton.widthAnchor.constraint(equalToConstant: 150),
+            nextPageButton.heightAnchor.constraint(equalToConstant: 40),
+            nextPageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextPageButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(layoutConstraints)
         
     }
+    
+    func stylize() {
+        view.backgroundColor = .white
+        
+        nextPageButton.setTitle("Next Page", for: .normal)
+        nextPageButton.tintColor = .white
+        nextPageButton.backgroundColor = .black
+        nextPageButton.layer.cornerRadius = 12
+    }
+    
+    func setActions() {
+        nextPageButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
 
-
+    @objc func buttonTapped() {
+        print("Button tapped")
+        let detailsVC = DetailsViewController()
+        navigationController?.pushViewController(detailsVC, animated: true)
+    }
 }
 
